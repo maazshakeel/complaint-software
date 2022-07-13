8ac4977d03a27a4849a2c2c87961c98f660c03c6
maazshakeel <maaz.shakeel.bodla@gmail.com>
2022-07-13 19:55:34 +0500

    total screens we need

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+// screens
+import LoginScreen from './screens/login-screen'
+
+const StackRoot = createNativeStackNavigator()
+
+const App = () => {
+  return (
+    <StackRoot.Navigator initialRouteName="Login">
+      <StackRoot.Screen name="Login" component={LoginScreen} />
+    </StackRoot.Navigator>
+  );
+}
+
+export default App
---
+++ src/screens/login-screen.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Heading, Center } from 'native-base'
+
+const LoginScreen: React.FC = () => {
+  return (
+    <Center>
+      <Heading>
+        LoginScreen
+      </Heading>
+    </Center>
+  );
+}
+
+export default LoginScreen
