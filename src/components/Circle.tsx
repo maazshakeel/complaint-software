e17637a25b848e61e5bfe7300cd17b5b9d06f3bf
maazshakeel <maaz.shakeel.bodla@gmail.com>
2022-07-17 21:20:49 +0500

    some changes resolved and ticket number is consant in the file

--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -1,13 +1,36 @@
-import React, { FC} from 'react'
-import {
-    View,
-    StyleSheet
-} from 'react-native'
+import React, { FC } from 'react'
+import { View } from 'react-native'
 
-const Circle: FC = () => {
+interface ICircleProps {
+  resolved: boolean
+}
+
+const Circle: FC<ICircleProps> = resolved => {
+  if (resolved) {
     return (
-
-    );
+      <View
+        style={{
+          backgroundColor: '#FC9700',
+          borderColor: '#FC9700',
+          width: 20,
+          height: 20,
+          borderRadius: 13
+        }}
+      />
+    )
+  } else {
+    return (
+      <View
+        style={{
+          backgroundColor: '#2FEB00',
+          borderColor: '#2FEB00',
+          width: 20,
+          height: 20,
+          borderRadius: 13
+        }}
+      />
+    )
+  }
 }
 
 export default Circle
