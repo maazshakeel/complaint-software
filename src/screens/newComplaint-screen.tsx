7b07e5dbfcf62d0b54ed41db8e1c4e60bb2cf189
maazshakeel <maaz.shakeel.bodla@gmail.com>
2022-07-18 02:30:36 +0500

    new complaint done

--- src/screens/newComplaint-screen.tsx
+++ src/screens/newComplaint-screen.tsx
@@ -8,69 +8,92 @@ import {
 } from 'react-native'
 import React from 'react'
 import ProblemCard from '../components/problemCard'
+import Icon from 'react-native-vector-icons/FontAwesome5'
 
 const { height, width } = Dimensions.get('screen')
 
 export default function NewComplaint() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.textHeading}>
-        Choose the category under which your complaint falls.
-      </Text>
-      <View
-        style={{
-          top: 140,
-          display: 'flex',
-          marginLeft: 10,
-          flexDirection: 'row',
-          flexWrap: 'wrap',
-          justifyContent: 'space-between'
-        }}
-      >
-        <TouchableOpacity>
-          <ProblemCard backcolor="#FFEFE0">
-            <Image source={require('../assets/cleaning.png')} />
-            <Text style={{ color: '#F1D1A8', fontSize: 13 }}>Cleaning</Text>
-          </ProblemCard>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <ProblemCard backcolor="#F0F8FA">
-            <Image source={require('../assets/Electrical.png')} />
-            <Text style={{ color: '#9ACFD4', fontSize: 13 }}>Electrical</Text>
-          </ProblemCard>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <ProblemCard backcolor="#FFEFE0">
-            <Image source={require('../assets/plumbing-icon.png')} />
-            <Text style={{ color: '#F1D1A8', fontSize: 13 }}>Plumbing</Text>
-          </ProblemCard>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <ProblemCard backcolor="#FEF6F6">
-            <Image source={require('../assets/network.png')} />
-            <Text style={{ color: '#FDCECE', fontSize: 13 }}>Network</Text>
-          </ProblemCard>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <ProblemCard backcolor="#F0F8FA">
-            <Image source={require('../assets/parking.png')} />
-            <Text style={{ color: '#9ACFD4', fontSize: 13 }}>Parking</Text>
-          </ProblemCard>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <ProblemCard backcolor="#F7F2F3">
-            <Image source={require('../assets/security.png')} />
-            <Text style={{ color: '#E1C6CC', fontSize: 13 }}>Security</Text>
-          </ProblemCard>
-        </TouchableOpacity>
-        <TouchableOpacity style={{ marginLeft: width / 2 - 55 }}>
-          <ProblemCard backcolor="#F7F2F3">
-            <Image source={require('../assets/three-dots.png')} />
-            <Text style={{ color: '#E1C6CC', fontSize: 13 }}>Others</Text>
-          </ProblemCard>
+    <>
+      <View style={styles.container}>
+        <Text style={styles.textHeading}>
+          Choose the category under which your complaint falls.
+        </Text>
+        <View
+          style={{
+            top: 140,
+            display: 'flex',
+            marginLeft: 10,
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+            justifyContent: 'space-between'
+          }}
+        >
+          <TouchableOpacity>
+            <ProblemCard backcolor="#FFEFE0">
+              <Image source={require('../assets/cleaning.png')} />
+              <Text style={{ color: '#F1D1A8', fontSize: 13 }}>Cleaning</Text>
+            </ProblemCard>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <ProblemCard backcolor="#F0F8FA">
+              <Image source={require('../assets/Electrical.png')} />
+              <Text style={{ color: '#9ACFD4', fontSize: 13 }}>Electrical</Text>
+            </ProblemCard>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <ProblemCard backcolor="#FFEFE0">
+              <Image source={require('../assets/plumbing-icon.png')} />
+              <Text style={{ color: '#F1D1A8', fontSize: 13 }}>Plumbing</Text>
+            </ProblemCard>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <ProblemCard backcolor="#FEF6F6">
+              <Image source={require('../assets/network.png')} />
+              <Text style={{ color: '#FDCECE', fontSize: 13 }}>Network</Text>
+            </ProblemCard>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <ProblemCard backcolor="#F0F8FA">
+              <Image source={require('../assets/parking.png')} />
+              <Text style={{ color: '#9ACFD4', fontSize: 13 }}>Parking</Text>
+            </ProblemCard>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <ProblemCard backcolor="#F7F2F3">
+              <Image source={require('../assets/security.png')} />
+              <Text style={{ color: '#E1C6CC', fontSize: 13 }}>Security</Text>
+            </ProblemCard>
+          </TouchableOpacity>
+          <TouchableOpacity style={{ marginLeft: width / 2 - 55 }}>
+            <ProblemCard backcolor="#F7F2F3">
+              <Image source={require('../assets/three-dots.png')} />
+              <Text style={{ color: '#E1C6CC', fontSize: 13 }}>Others</Text>
+            </ProblemCard>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={{ alignItems: 'center' }}>
+        <TouchableOpacity
+          style={{
+            backgroundColor: '#2F5FE3',
+            width: 311,
+            height: 54,
+            borderRadius: 10,
+            alignItems: 'center',
+            justifyContent: 'center',
+            bottom: 30,
+            display: 'flex',
+            flexDirection: 'row'
+          }}
+        >
+          <Text style={{ fontSize: 18, color: '#FFF', marginRight: 7 }}>
+            Next
+          </Text>
+          <Icon name="caret-right" size={27} />
         </TouchableOpacity>
       </View>
-    </View>
+    </>
   )
 }
 
