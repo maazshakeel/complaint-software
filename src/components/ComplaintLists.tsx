e09b02ea6f5259156b1793256ac10729f63073ca
maazshakeel <maaz.shakeel.bodla@gmail.com>
2022-07-17 18:15:22 +0500

    Basic strucutre

---
+++ src/components/ComplaintLists.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react'
+import { ScrollView } from 'react-native'
+
+const ComplaintList: FC = () => {
+  return (
+    <ScrollView>
+      {/* Row  */}
+      <Text></Text>
+    </ScrollView>
+  )
+}
