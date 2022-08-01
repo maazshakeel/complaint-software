1c903cbbd5f7b69bdf07bbf7cbea969ef34b67bf
maazshakeel <maaz.shakeel.bodla@gmail.com>
2022-08-02 00:37:12 +0500

    fix: generate random five digit number now

--- src/utils/random-ticket-no.ts
+++ src/utils/random-ticket-no.ts
@@ -2,7 +2,7 @@ const fiveDigitNo = () => {
 
   // min and max
   let min: number = 10000
-  let max: number = 99999
+  let max: number = 90000
 
   // generate random number of 5 digits
   let randomNum: number = Math.floor(Math.random() * min) + max
